f8cc750f444aba593199f012f3657e790017721e
Vasily Guzov <vasily.guzov@gmail.com>
2020-08-08 11:38:03 +0300

    setup prettier and eslint

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,29 @@ module.exports = {
     author: `Vasily Guzov`,
   },
   plugins: [
+    {
+      resolve: "gatsby-plugin-prettier-eslint",
+      options: {
+        prettier: {
+          patterns: [
+            // the pattern "**/*.{js,jsx,ts,tsx}" is not used because we will rely on `eslint --fix`
+            "**/*.{css,scss,less}",
+            "**/*.{json,json5}",
+            "**/*.{graphql}",
+            "**/*.{md,mdx}",
+            "**/*.{html}",
+            "**/*.{yaml,yml}",
+          ],
+        },
+        eslint: {
+          patterns: "**/*.{js,jsx,ts,tsx}",
+          customOptions: {
+            fix: true,
+            cache: true,
+          },
+        },
+      },
+    },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
