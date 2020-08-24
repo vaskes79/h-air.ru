1447475f0f138a4ce232096ac1a3a48fd083b148
Vasily Guzov <vasily.guzov@gmail.com>
2020-08-24 18:49:15 +0300

    create static page for jobs

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,55 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require(`path`);
 
-// You can delete this file if you're not using it
+const makeRequest = (graphql, request) =>
+  new Promise((resolve, reject) => {
+    resolve(
+      graphql(request).then(result => {
+        if (result.errors) {
+          reject(result.errors);
+        }
+
+        return result;
+      })
+    );
+  });
+
+exports.createPages = ({ actions, graphql }) => {
+  const { createPage } = actions;
+
+  const getJob = makeRequest(
+    graphql,
+    `
+    {
+      allStrapiJob {
+        edges {
+          node {
+          id
+          slug
+          published
+          }
+        }
+      }
+    }
+    `
+  )
+    .then(result => {
+      result.data.allStrapiJob.edges.forEach(({ node }) => {
+        const { slug, id, published } = node;
+
+        if (published) {
+          console.log(`page: ${slug} id: ${id}, published: ${published}`);
+          createPage({
+            path: `/jobs/${slug}`,
+            component: path.resolve(`src/templates/job-page.js`),
+            context: {
+              id,
+              slug,
+            },
+          });
+        }
+      });
+    })
+    .catch(err => console.log("Page Creating jobs", err));
+
+  return Promise.all([getJob]);
+};
---
+++ src/templates/job-page.js
@@ -0,0 +1,34 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { graphql } from "gatsby";
+
+const JobPage = ({ data }) => <pre>{JSON.stringify(data, null, 4)}</pre>;
+
+export const query = graphql`
+  query JobQuery($id: String!) {
+    strapiJob(id: { eq: $id }) {
+      title
+      slug
+      youtube
+      description
+      photo {
+        childImageSharp {
+          fluid {
+            srcSet
+          }
+        }
+      }
+      photos {
+        photos {
+          url
+        }
+      }
+    }
+  }
+`;
+
+JobPage.propTypes = {
+  data: PropTypes.obj,
+};
+
+export default JobPage;
